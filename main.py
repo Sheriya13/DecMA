9189c7c9978fe895a1d46a7bc3975af249033c49
SCP002 <22541678+SCP002@users.noreply.github.com>
2026-03-03 14:50:55 +0300

    Run black formatter on main file

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main_menu():
 
 
 def clr():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
 
 
 def xor(data, key):
@@ -17,9 +17,11 @@ def xor(data, key):
         modif_data[i] = data[i] ^ key[i % len(key)]
     return modif_data
 
+
 def file_name_input():
     f_name = input("\n\t\tFile name: ")
     return f_name
+
 
 def key_input():
     try:
@@ -33,18 +35,19 @@ def key_input():
         print(e)
         return
 
+
 def file_process(f_name, key_str, f_mode):
     if key_str is None:
         return
-    
-    key = key_str.encode('utf-8')
-    
+
+    key = key_str.encode("utf-8")
+
     try:
         with open(f_name, "rb") as f_in:
             data = f_in.read()
-        
+
         modif_data = xor(data, key)
-        
+
         if f_mode == "encrypt":
             output_name = f"{f_name}.enc"
         else:
@@ -53,41 +56,42 @@ def file_process(f_name, key_str, f_mode):
             if not os.path.exists(decrypt_dir):
                 os.mkdir(decrypt_dir)
             output_name = os.path.join(decrypt_dir, os.path.basename(f_name.replace(".enc", "")))
-            
+
         with open(output_name, "wb") as f_out:
             f_out.write(modif_data)
-        
+
         clr()
         print(f"\n\t\tSuccesfully {f_mode}ed")
-    
+
     except FileNotFoundError:
         print("\n\t\t File not found")
 
 
 def main():
-    
+
     if len(sys.argv) > 1:
-        parser = argparse.ArgumentParser(description= "Python file encryption utility")
-        parser.add_argument("--mode", choices=["encrypt", "decrypt"], required=True, type= str, help = "Program operating mode")
-        parser.add_argument("--path", type= str, required=True, help="File location")
-        parser.add_argument("--key", type= str, required=True, help="Key for encrypt or decrypt")
+        parser = argparse.ArgumentParser(description="Python file encryption utility")
+        parser.add_argument(
+            "--mode", choices=["encrypt", "decrypt"], required=True, type=str, help="Program operating mode"
+        )
+        parser.add_argument("--path", type=str, required=True, help="File location")
+        parser.add_argument("--key", type=str, required=True, help="Key for encrypt or decrypt")
         args = parser.parse_args()
-        
+
         if args.mode == "encrypt":
             file_process(args.path, args.key, args.mode)
             return
         elif args.mode == "decrypt":
             file_process(args.path, args.key, args.mode)
             return
-    
-    
+
     while True:
         main_menu()
         while True:
             try:
                 so = int(input("\n\t\tSo: "))
                 break
-            except ValueError: 
+            except ValueError:
                 clr()
                 print("\n\t\t!!!Invalid value")
                 main_menu()
@@ -99,7 +103,8 @@ def main():
             case 2:
                 clr()
                 file_process(file_name_input(), key_input(), "decrypt")
-            case 3: return
+            case 3:
+                return
             case _:
                 clr()
                 print("\n\t\t!!!Invalid number")
